51d41ec3a1754045d7198d5b0748ebb9d43c68ff
haritsf <haritsf@hotmail.com>
2022-08-17 22:24:41 +0700

    New Component for Video Preview

---
+++ components/Invitation/VideoPreview.jsx
@@ -0,0 +1,11 @@
+const VideoPreview = () => {
+  return (
+    <video
+      controls
+      src={"/MiftahEmir-Engage.mp4"}
+    //   style={{ width: "400px", height: "400px" }}
+    />
+  );
+};
+
+export default VideoPreview;
