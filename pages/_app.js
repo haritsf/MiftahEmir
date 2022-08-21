a6374ec20370993538336d437b483634f874f6db
haritsf <haritsf@hotmail.com>
2022-08-21 17:15:49 +0700

    Add toast Library

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <ToastContainer position="top-right" autoClose={3000} />
       <Head>
         <title>#FirdausilJannati</title>
         <meta name="title" content="#FirdausilJannati" />
